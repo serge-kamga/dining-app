fbf83591bb606c07e4e7d7698b22d4095bba2e8b
Serge Kamga <serge_h.kamga@yahoo.com>
2020-12-20 00:14:42 -0800

    first commit

---
+++ dining-ios-app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  dining-ios-app
+//
+//  Created by Serge Kamga on 12/20/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
